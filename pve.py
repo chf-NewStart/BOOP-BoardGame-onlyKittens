236d4270376296a5ae2d0f0da0b8325e34d95917
chf-NewStart <houfu.chen@mail.utoronto.ca>
2025-02-28 18:00:52 -0500

    Tracked .p files using Git LFS

--- pve.py
+++ pve.py
@@ -1,18 +1,3 @@
-import numpy as np
-import tkinter as tk
-import copy
-import pickle as pickle    # cPickle is available in Python 2.x only, otherwise use pickle
-
-from qo import Game, HumanPlayer, QPlayer
-
-
-Q = pickle.load(open("Q_epsilon_09_Nepisodes_10000.p", "rb"))
-
-root = tk.Tk()
-player1 = HumanPlayer(mark="X")
-player2 = QPlayer(mark="O", epsilon=0)
-
-game = Game(root, player1, player2, Q=Q)
-
-game.play()
-root.mainloop()
+version https://git-lfs.github.com/spec/v1
+oid sha256:04da7feda08aac4ef4316bff2290dd567e6bcf401745b2f869e9ead52a6d582d
+size 407
--- trainQ.py
+++ trainQ.py
@@ -1,21 +1,3 @@
-import numpy as np
-import tkinter as tk
-import copy
-import pickle
-from qo import Game, QPlayer     # Classes used for BOOP
-
-root = tk.Tk()
-epsilon = 0.9
-player1 = QPlayer(mark="X",epsilon = epsilon)
-player2 = QPlayer(mark="O",epsilon = epsilon)
-game = Game(root, player1, player2)
-
-N_episodes = 100000
-for episodes in range(N_episodes):
-    game.play()
-    game.reset()
-
-Q = game.Q
-
-filename = "Q_epsilon_09_Nepisodes_{}.p".format(N_episodes)
-pickle.dump(Q, open(filename, "wb"))
+version https://git-lfs.github.com/spec/v1
+oid sha256:0133b3dff1499d39e101e8ef6334bc31b0276825b13be6d51f4da414ad9191d1
+size 479
